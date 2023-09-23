4e48007c2dab0d99194449806f9ab7702180aa4e
David Attah-Ogbole <d.attahogbole@gmail.com>
2023-09-23 16:04:09 -0400

    "Added variables to track the scores' Created a variablecalled playerScore to track how many wins the player has created a variable called computerScore to track how many wins the computer has

--- project1.js
+++ project1.js
@@ -23,23 +23,39 @@ function getPlayerChoice(){
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 function playSingleRound(playerSelection, computerSelection){
      if(playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore +=1;
         return "You win! Rock beats Scissors"
      }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        computerScore +=1;
         return "You lose! Scissors beats Rock"
      }
 
      if(playerSelection == "paper" && computerSelection == "rock"){
+        playerScore +=1;
         return "You win! Paper beats Rock"
      }else if(playerSelection == "rock" && computerSelection == "paper"){
+        computerScore +=1;
         return "You lose! Paper beats Rock"
      }
 
      if(playerSelection == "scissors" && computerSelection =="paper"){
+        playerScore +=1;
         return "You win! Scissors beats paper"
      }else if(playerSelection == "paper" && computerSelection =="scissors"){
+        computerScore +=1;
         return "You lose! Scissors beats paper"
+     }
+
+     if( playerSelection == computerSelection){
+        return "It is a tie!"
      }
 }
 
+let numberOfRounds = 0;
+function game(){
+
+}
