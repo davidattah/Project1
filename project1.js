7118cd51c3f53111dc3d0302acb5701597cdaadf
David Attah-Ogbole <d.attahogbole@gmail.com>
2023-10-16 12:10:43 -0400

    "Added an event listener for the buttons" Created a variable and assigned it to the nodelist for all the buttons Used a for each loop to go through the nodelist as it is an array -like object Created an event listener for each button in the nodelist Assigned the id of each button to buttonClicked Created an if statement to check what button was liked and assigned the answer as the player selection

--- project1.js
+++ project1.js
@@ -8,56 +8,57 @@ function getComputerChoice(){
      return computerSelection = "scissors"
    }
 }
-
-function getPlayerChoice(){
-    let condition = true;
-    playerSelection = prompt("Rock, paper or scissors?");
-    while(condition == true)
-    if(playerSelection.toLowerCase() === "rock" || playerSelection.toLowerCase() === "paper" || playerSelection.toLowerCase() === "scissors"){
-        condition = false;
-        playerSelection = playerSelection.toLowerCase()
-        alert("Thank you")
-        return playerSelection;
-    }else{
-         alert("Not a valid option")
-         getPlayerChoice()
-    }
-} 
-
-let playerScore = 0;
-let computerScore = 0;
-function playSingleRound(playerSelection, computerSelection){
-     if(playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore +=1;
-        console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
-     }else if(playerSelection === "paper" && computerSelection === "rock"){
-        playerScore +=1;
-        console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
-     }else if(playerSelection === "scissors" && computerSelection === "paper"){
-        playerScore +=1;
-        console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
-    }else if(computerSelection=== "rock" && playerSelection === "scissors"){
-        computerScore +=1;
-        console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
-    }else if(computerSelection === "paper" && playerSelection === "rock"){
-            computerScore +=1;
-            console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`)   
-    }else if(computerSelection === "scissors" && playerSelection === "paper"){
-            computerScore +=1;
-            console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
-    }else if(playerSelection === computerSelection || computerSelection === playerSelection){
-        console.log(`It is a tie! ${playerScore} : ${computerScore}.`)
-     } 
-    }
-
-    const body = document.querySelector("body")
-    const rockButton = document.createElement("button")
-    const scissorsButton = document.createElement("button")
-    const paperButton = document.createElement("button")
-    body.appendChild(rockButton)
-    body.appendChild(scissorsButton)
-    body.appendChild(paperButton)
+    let playerScore = 0;
+    let computerScore = 0;
+    function playSingleRound(playerSelection, computerSelection){
+        if(playerSelection === "rock" && computerSelection === "scissors") {
+           playerScore +=1;
+           console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+        }else if(playerSelection === "paper" && computerSelection === "rock"){
+           playerScore +=1;
+           console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+        }else if(playerSelection === "scissors" && computerSelection === "paper"){
+           playerScore +=1;
+           console.log(`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+       }else if(computerSelection=== "rock" && playerSelection === "scissors"){
+           computerScore +=1;
+           console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
+       }else if(computerSelection === "paper" && playerSelection === "rock"){
+               computerScore +=1;
+               console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`)   
+       }else if(computerSelection === "scissors" && playerSelection === "paper"){
+               computerScore +=1;
+               console.log(`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
+       }else if(playerSelection === computerSelection || computerSelection === playerSelection){
+           console.log(`It is a tie! ${playerScore} : ${computerScore}.`)
+        } 
+       }
+   
+       const body = document.querySelector("body")
+       const rockButton = document.createElement("button")
+       const scissorsButton = document.createElement("button")
+       const paperButton = document.createElement("button")
+       body.appendChild(rockButton)
+       body.appendChild(scissorsButton)
+       body.appendChild(paperButton)
+   
+       rockButton.id = "rock"
+       scissorsButton.id = "scissors"
+       paperButton.id = "paper"
+   
+       const buttons = document.querySelectorAll("button")
+       buttons.forEach((button)=>{
+           button.addEventListener("click",(event)=>{
+               buttonClicked = event.target.id
+               if(buttonClicked === rockButton.id){
+                   playerSelection = "rock"
+               }else if(buttonClicked === scissorsButton.id){
+                   playerSelection = "scissors"
+               }else if(buttonClicked === paperButton.id){
+                   playerSelection = "paper"
+               }
+               playSingleRound(buttonClicked,getComputerChoice())
+           })
+          
+       })
     
-    
-
-
