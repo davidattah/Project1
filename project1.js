049a6fb0a421c23d81ea0812f6b7a1253efb294c
David Attah-Ogbole <d.attahogbole@gmail.com>
2023-11-15 14:42:34 -0500

    "Modified game to include reset button" Added new divs for the buttons and the result of the game. Added a new button to enable players to reset the game if they choose to do so Added new classes to able to style in CSS

--- project1.js
+++ project1.js
@@ -13,53 +13,68 @@ function getComputerChoice(){
 function playSingleRound(playerSelection, computerSelection){
         if(playerSelection === "rock" && computerSelection === "scissors") {
            playerScore +=1;
-           div.textContent = (`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+           result.textContent = (`You win ${playerSelection} beats ${computerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
         }else if(playerSelection === "paper" && computerSelection === "rock"){
            playerScore +=1;
-           div.textContent = (`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+           result.textContent = (`You win ${playerSelection} beats ${computerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
         }else if(playerSelection === "scissors" && computerSelection === "paper"){
            playerScore +=1;
-           div.textContent = (`You win ${playerSelection} beats ${computerSelection}! ${playerScore} : ${computerScore}.`)
+           result.textContent = (`You win ${playerSelection} beats ${computerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
        }else if(computerSelection=== "rock" && playerSelection === "scissors"){
            computerScore +=1;
-           div.textContent = (`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
+           result.textContent = (`You lose ${computerSelection} beats ${playerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`) 
        }else if(computerSelection === "paper" && playerSelection === "rock"){
                computerScore +=1;
-               div.textContent = (`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`)   
+               result.textContent = (`You lose ${computerSelection} beats ${playerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)   
        }else if(computerSelection === "scissors" && playerSelection === "paper"){
                computerScore +=1;
-               div.textContent = (`You lose ${computerSelection} beats ${playerSelection}! ${playerScore} : ${computerScore}.`) 
+               result.textContent = (`You lose ${computerSelection} beats ${playerSelection}!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`) 
        }else if(playerSelection === computerSelection || computerSelection === playerSelection){
-                div.textContent = (`It is a tie! ${playerScore} : ${computerScore}.`)
+        result.textContent = (`It is a tie!  Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
         } 
 
         if(playerScore == 5){
-            div.textContent = (`You win! ${playerScore} : ${computerScore}.`)
-            playerScore = 0;
-            computerScore = 0;
+            result.textContent = (`You win! Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
+            reset();
           }else if(computerScore == 5){
-            div.textContent = (`You lose! ${playerScore} : ${computerScore}.`)
-            playerScore = 0;
-            computerScore = 0;
+            result.textContent = (`You lose! Player Score : ${playerScore} vs Computer Score : ${computerScore}.`)
+            reset();
           }
     
        }
-   
+
+       function reset(){
+            playerScore = 0
+            computerScore = 0;
+       }
+       
        const body = document.querySelector("body")
+       const container = document.createElement("div")
+       container.classList.add("container")
+       body.appendChild(container)
+
+       const buttonContainer = document.createElement("div") 
+       buttonContainer.classList.add("button-container")
+       container.appendChild(buttonContainer)
+
        const rockButton = document.createElement("button")
        const scissorsButton = document.createElement("button")
        const paperButton = document.createElement("button")
-       body.appendChild(rockButton)
-       body.appendChild(scissorsButton)
-       body.appendChild(paperButton)
+       const resetButton = document.createElement("button")
+       buttonContainer.appendChild(rockButton)
+       buttonContainer.appendChild(scissorsButton)
+       buttonContainer.appendChild(paperButton)
+       buttonContainer.appendChild(resetButton)
 
        rockButton.textContent = "Rock"
        scissorsButton.textContent = "Scissors"
        paperButton.textContent = "Paper"
+       resetButton.textContent = "Reset"
    
        rockButton.id = "rock"
        scissorsButton.id = "scissors"
        paperButton.id = "paper"
+       resetButton.id= "reset"
    
        const buttons = document.querySelectorAll("button")
        buttons.forEach((button)=>{
@@ -71,13 +86,21 @@ function playSingleRound(playerSelection, computerSelection){
                    playerSelection = "scissors"
                }else if(buttonClicked === paperButton.id){
                    playerSelection = "paper"
+               }else if(buttonClicked = resetButton.id){
+                reset();
+                result.textContent = ""
                }
                playSingleRound(buttonClicked,getComputerChoice())
            })
        })
 
-       const div = document.createElement("div")
-       body.appendChild(div)
+       const resultContainer = document.createElement("div")
+       container.appendChild(resultContainer)
+       resultContainer.classList.add("result-container")
+       const result = document.createElement("div")
+       result.classList.add("result")
+       resultContainer.appendChild(result)
+
 
 
   
